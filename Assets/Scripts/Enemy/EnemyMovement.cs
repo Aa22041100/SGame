89ccedafea792a5e8bab07ea07b35a6b6c176fab
Stanley Lai <stanleylai@juicyapphk.com>
2018-06-14 17:30:55 +0800

    added basic Enemy AI (movement only)

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour {
+
+
+	NavMeshAgent navAgent;
+	Transform player;
+	EnemyHealth health;
+
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	void Awake() {
+	}
+
+	// Use this for initialization
+	void Start () {
+		navAgent = GetComponent<NavMeshAgent> ();
+		health = GetComponent<EnemyHealth> ();
+		player = GameManager.Instance.player.GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		navAgent.SetDestination (player.position);
+	}
+}
